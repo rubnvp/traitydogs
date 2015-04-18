88f2dca2a668d163210d86007eb87fbe981c9e3f
Ruben <rubn.vp@gmail.com>
2015-04-18 21:52:59 +0200

    implemented parse getDogInformation

--- public/js/traitydogs.js
+++ public/js/traitydogs.js
@@ -1,10 +1,5 @@
-$(function() {
-  
-   $('.thumbnail').on('click', function(e) {
-      e.preventDefault();
-      $('#modalShowDog').modal('toggle');
-   });   
-
+$(function() {   
+// ----- Get dogs to show on dogs gallery
    function getDogs(){
       var query = new Parse.Query("Dog");
       query.include("user");
@@ -17,6 +12,7 @@ $(function() {
                gallery += getDogThumbnail(dogId, photoUrl);
             });
             $(".dog-gallery").html(gallery);
+            dogs
          },
          error: function(error) {
          // The request failed
@@ -26,13 +22,77 @@ $(function() {
    }
 
    function getDogThumbnail(dogId, photoUrl){
-      return '<div class="col-lg-3 col-md-4 col-xs-6"><a class="thumbnail" href="#"><img class="img-responsive" data-id="'+dogId+'" src="'+photoUrl+'" alt=""></a></div>';
+      return '<div class="col-lg-3 col-md-4 col-xs-6"><a class="thumbnail" data-id="'+dogId+'" href="#"><img class="img-responsive" src="'+photoUrl+'" alt=""></a></div>';
    }
 
+// ----- Get a dog to show on Modal window
+   $(document).on('click', '.thumbnail', function(e){
+      e.preventDefault();
+      var dogId = $(this).attr("data-id");
+      getDogInformation(dogId);
+      $('#dog-modal').modal('toggle');
+   }); 
+
+   function getDogInformation(dogId){
+      var query = new Parse.Query("Dog");
+      query.include("user");
+      query.get(dogId, {
+         success: function(dog) {            
+            var name = dog.get("name");
+            var photoUrl = dog.get("photo");
+            var gender = dog.get("gender");
+            var races = dog.get("race");
+            var race = races[0];
+            var pedigree = dog.get("pedigree");
+            var pedigree = pedigree ? "Con pedigree" : "Sin pedigree";
+            var bornDate = dog.get("born");
+            var age = getAge(bornDate); 
+            var city = dog.get("city");
+            var user = dog.get("user");
+            var contact = getContact(user);
+            showDogModalWindow(name, photoUrl, gender, race, pedigree, age, city, contact);
+         },
+         error: function(error) {
+            // The request failed
+            console.error("Error: " + error.code + " " + error.message);
+         }
+      });
+   }
+
+   function showDogModalWindow(name, photoUrl, gender, race, pedigree, age, city, contact){
+      $("#dog-modal .dog-name").html(name);
+      $("#dog-modal .dog-image").attr("src", photoUrl);
+      var informationList = getDogInformationList(gender, race, pedigree, age, city, contact);
+      $("#dog-modal .dog-information").html(informationList);
+   }
+
+   function getDogInformationList(gender, race, pedigree, age, city, contact){
+      var list = "";
+      list += '<li class="list-group-item">'+gender+'</li>';
+      list += '<li class="list-group-item">'+race+'</li>';
+      list += '<li class="list-group-item">'+pedigree+'</li>';
+      list += '<li class="list-group-item">'+age+'</li>';
+      list += '<li class="list-group-item">'+city+'</li>';
+      list += '<li class="list-group-item">'+contact+'</li>';
+      return list;
+   }
+
+   function getAge(bornDate){
+      var dateToday = new Date();
+      var diferenceWithToday = 3+" años";//moment(dateToday).diff(moment(bornDate)).format("YY")+" años";
+      return diferenceWithToday;
+   }
+
+   function getContact(user){
+      var name = user.get("username");
+      var email = user.get("email");
+      return name+" ("+email+")";
+   }
+
+// ---- Initialize
    function initialize(){
       Parse.initialize("D9V5hkDmqPf2beWiXe2oyHohNLqghx5FmoyY11th", "oTKz3eemuJo0r9njdcD89btqHAxuw5lRIFa0YPs5");
       getDogs();
    }
-
    initialize();
 });
